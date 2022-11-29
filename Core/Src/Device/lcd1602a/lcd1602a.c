71e14430115040a4e3dd235c250816a1fa878376
The Level Three Chin <henchelsantillan@gmail.com>
2022-11-28 21:13:02 -0500

    [WIP][LUNA-35] LCD Driver Bring-Up: Restructure device lib files

--- Core/Src/Device/lcd1602a/lcd1602a.c
+++ Core/Src/Device/lcd1602a/lcd1602a.c
@@ -1,9 +1,87 @@
 #include "lcd1602a.h"
-#include "lcd1602aConfig.h"
 
 
-uint32_t Init_LCD1602A() {
-	// Turn on device
+/*** STATIC HELPER FUNCTIONS ***/
+
+static void write_4bits(LCD_TypeDef *p_lcd, uint8_t value) {
+	// Concerned with the LSB; therefore, write to the lower nibble ("half-byte")
+	HAL_GPIO_WritePin(p_lcd->D4->GPIO_PERIPHERAL, p_lcd->D4->GPIO_PIN, (value & 0x08));
+	HAL_GPIO_WritePin(p_lcd->D5->GPIO_PERIPHERAL, p_lcd->D5->GPIO_PIN, (value & 0x04));
+	HAL_GPIO_WritePin(p_lcd->D6->GPIO_PERIPHERAL, p_lcd->D6->GPIO_PIN, (value & 0x02));
+	HAL_GPIO_WritePin(p_lcd->D7->GPIO_PERIPHERAL, p_lcd->D7->GPIO_PIN, (value & 0x01));
+}
+
+static void write_8bits(LCD_TypeDef *p_lcd, uint8_t value) {
+	HAL_GPIO_WritePin(p_lcd->D0->GPIO_PERIPHERAL, p_lcd->D0->GPIO_PIN, (value & 0x80));
+	HAL_GPIO_WritePin(p_lcd->D1->GPIO_PERIPHERAL, p_lcd->D1->GPIO_PIN, (value & 0x40));
+	HAL_GPIO_WritePin(p_lcd->D2->GPIO_PERIPHERAL, p_lcd->D2->GPIO_PIN, (value & 0x20));
+	HAL_GPIO_WritePin(p_lcd->D3->GPIO_PERIPHERAL, p_lcd->D3->GPIO_PIN, (value & 0x10));
+	HAL_GPIO_WritePin(p_lcd->D4->GPIO_PERIPHERAL, p_lcd->D4->GPIO_PIN, (value & 0x08));
+	HAL_GPIO_WritePin(p_lcd->D5->GPIO_PERIPHERAL, p_lcd->D5->GPIO_PIN, (value & 0x04));
+	HAL_GPIO_WritePin(p_lcd->D6->GPIO_PERIPHERAL, p_lcd->D6->GPIO_PIN, (value & 0x02));
+	HAL_GPIO_WritePin(p_lcd->D7->GPIO_PERIPHERAL, p_lcd->D7->GPIO_PIN, (value & 0x01));
 }
 
 
+/*** FUNCTION DEFINITIONS ***/
+
+void Init_LCD1602A(LCD_TypeDef *p_lcd, Pin_TypeDef *rs, Pin_TypeDef *rw, Pin_TypeDef *e,
+
+#ifdef MODE_EIGHT_BIT
+		Pin_TypeDef *D0, Pin_TypeDef *D1, Pin_TypeDef *D2, Pin_TypeDef *D3,
+#endif
+		Pin_TypeDef *D4, Pin_TypeDef *D5, Pin_TypeDef *D6, Pin_TypeDef *D7) {
+
+	p_lcd->RS = rs;
+	p_lcd->RW = rw;
+	p_lcd->E = e;
+
+	p_lcd->D4 = D4;
+	p_lcd->D5 = D5;
+	p_lcd->D6 = D6;
+	p_lcd->D7 = D7;
+
+#ifdef MODE_EIGHT_BIT
+	p_lcd->D0 = D0;
+	p_lcd->D1 = D1;
+	p_lcd->D2 = D2;
+	p_lcd->D3 = D3;
+
+	// Set the LCD to be 8-Bit Mode
+	send_command(p_lcd, CMD_FUNCTIONSET | FL_FUNCTIONSET_8BITMODE | FL_FUNCTIONSET_TWOLINE | FL_FUNCTIONSET_5X8MODE);
+
+#else
+	// Set the LCD to be 4-Bit Mode
+	send_command(p_lcd, CMD_FUNCTIONSET | FL_FUNCTIONSET_4BITMODE | FL_FUNCTIONSET_TWOLINE | FL_FUNCTIONSET_5X8MODE);
+
+#endif
+
+	// Set the Display, Blink, and Cursor to be all ON
+	send_command(p_lcd, CMD_DISPLAYCONTROL | FL_DISPLAYCONTROL_DISPLAYON | FL_DISPLAYCONTROL_CURSORON | FL_DISPLAYCONTROL_BLINKON);
+
+	send_command(p_lcd, CMD_ENTRYMODESET | FL_ENTRYMODESET_SHIFTRIGHT | FL_ENTRYMODESET_DDRAMDECR);
+}
+
+void send_command(LCD_TypeDef *p_lcd, uint8_t command) {
+	// To send commands, must pull RS and RW both to LOW
+	HAL_GPIO_WritePin(p_lcd->RS->GPIO_PERIPHERAL, p_lcd->RS->GPIO_PIN, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(p_lcd->RW->GPIO_PERIPHERAL, p_lcd->RW->GPIO_PIN, GPIO_PIN_RESET);
+
+#ifdef MODE_EIGHT_BITS
+	write_8bits(p_lcd, command);
+#else
+	write_4bits(p_lcd, command);
+#endif
+}
+
+void send_data(LCD_TypeDef *p_lcd, uint8_t data) {
+	// To send data, the RS pin must be pulled HIGH, and the RW pin must be pulled LOW
+	HAL_GPIO_WritePin(p_lcd->RS->GPIO_PERIPHERAL, p_lcd->RS->GPIO_PIN, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(p_lcd->RW->GPIO_PERIPHERAL, p_lcd->RW->GPIO_PIN, GPIO_PIN_RESET);
+
+#ifdef MODE_EIGHT_BITS
+	write_4bits(p_lcd, data);
+#else
+	write_8bits(p_lcd, data);
+#endif
+}
--- Core/Src/Device/lcd1602a/lcd1602a.h
+++ Core/Src/Device/lcd1602a/lcd1602a.h
@@ -1,9 +1,81 @@
 #ifndef LUNASYS_LCD1602A_LCD1602A_H
 #define LUNASYS_LCD1602A_LCD1602A_H
 
+#include "lcd1602aConfig.h"
+#include "stm32f4xx_hal.h"
 
-uint32_t Init_LCD1602A();
-void Clear_Display_LCD1602A();
+#include <stdint.h>
+
+// Uncomment to use eight-bit mode
+// #define MODE_EIGHT_BIT
+
+
+/*** Common Device Library structures ***/
+
+typedef struct _Pin_TypeDef {
+
+	GPIO_TypeDef *GPIO_PERIPHERAL;	// GPIOx, x = { A, ..., K }
+	uint32_t GPIO_PIN;				// GPIO_Pin_x, x = { 0, ..., 15 }
+
+} Pin_TypeDef;
+
+
+typedef struct _LCD_TypeDef {
+
+	Pin_TypeDef *RS;	// Register Select
+	Pin_TypeDef *RW;	// Read-Write
+	Pin_TypeDef *E;		// Enable
+
+#ifdef MODE_EIGHT_BIT
+	Pin_TypeDef *D0, *D1, *D2, D3*;		// MSB - Data Pins
+#endif
+
+	Pin_TypeDef *D4, *D5, *D6, *D7;		// LSB - Data Pins
+
+
+} LCD_TypeDef;
+
+
+/*** FUNCTIONS ***/
+
+void Init_LCD1602A(LCD_TypeDef *p_lcd, Pin_TypeDef *rs, Pin_TypeDef *rw, Pin_TypeDef *e,
+
+#ifdef MODE_EIGHT_BIT
+		Pin_TypeDef *D0, Pin_TypeDef *D1, Pin_TypeDef *D2, Pin_TypeDef *D3,
+#endif
+		Pin_TypeDef *D4, Pin_TypeDef *D5, Pin_TypeDef *D6, Pin_TypeDef *D7);
+
+void Clear_Display_LCD1602A(LCD_TypeDef *p_lcd);
+
+void Return_Home_LCD1602A(LCD_TypeDef *p_lcd);
+
+void Shift_Cursor_LCD1602A(LCD_TypeDef *p_lcd);
+
+void Write_Char_LCD1602A(LCD_TypeDef *p_lcd, uint8_t *ch);
+
+void Write_String_LCD1602A(LCD_TypeDef *p_lcd, uint8_t *str);
+
+void Display_Off_LCD1602A(LCD_TypeDef *p_lcd);
+
+void Display_On_LCD1602A(LCD_TypeDef *p_lcd);
+
+void Blink_Off_LCD1602A(LCD_TypeDef *p_lcd);
+
+void Blink_On_LCD1602A(LCD_TypeDef *p_lcd);
+
+void Scroll_Left_LCD1602A(LCD_TypeDef *p_lcd);
+
+void Scroll_Right_LCD1602A(LCD_TypeDef *p_lcd);
+
+
+
+/* Users of this Device Library should generally not have to call these functions, and it is advised to use
+ * the Device Library functions suffixed with "LCD1602A" instead. Users that do choose to use these functions should
+ * include "lcd1602aConfig.h" and use the predefined command and flag macros.
+ *  */
+
+void send_command(LCD_TypeDef *p_lcd, uint8_t command);
+void send_data(LCD_TypeDef *p_lcd, uint8_t data);
 
 
 #endif // LUNASYS_LCD1602A_LCD1602A_H
